8c5f9eb7aae9bdce633f5251ee2ca1ad840f5319
Varvara <varvara.prokhvatylo@gmail.com>
2024-08-22 00:39:06 +0300

    added Footer

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,20 +1,25 @@
 import Typography from '@mui/material/Typography'
 import logo from '../../assets/logo-mountain.png'
 import './Logo.css'
+import { Link } from 'react-router-dom'
 
 type Props = {}
 const Logo = (props: Props) => {
     return (
         <>
-            <Typography
-                className="logo-img"
-                variant="h6"
-                component="div"
-                sx={{ flexGrow: 1 }}
-            >
-                <img src={logo} alt="" />
-                <Typography className="logo-text">Adventure Begins</Typography>
-            </Typography>
+            <Link to="/">
+                <Typography
+                    className="logo-img"
+                    variant="h6"
+                    component="div"
+                    sx={{ flexGrow: 1 }}
+                >
+                    <img src={logo} alt="" />
+                    <Typography className="logo-text">
+                        Adventure Begins
+                    </Typography>
+                </Typography>
+            </Link>
         </>
     )
 }
--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -1,5 +1,50 @@
+import { Container, List, ListItem } from '@mui/material'
+import instagram from '../assets/logo_instagram.png'
+import facebook from '../assets/logo-facebook.png'
+import twitter from '../assets/logo-twitter.png'
+import './Footer.css'
+
 type Props = {}
 const Footer = (props: Props) => {
-    return <div>Footer</div>
+    return (
+        <>
+            <Container className="footer">
+                <Container className="footer-main-container">
+                    <Container className="footer-media">
+                        <a href="https://www.instagram.com/feeriamandriv/">
+                            <img src={instagram} className="media-logo" />
+                        </a>
+                        <a href="https://www.facebook.com/feeria.ua">
+                            <img src={facebook} className="media-logo" />
+                        </a>
+                        <a href="https://x.com/FMandriv">
+                            <img src={twitter} className="media-logo" />
+                        </a>
+                    </Container>
+                    <Container className="footer-links-container">
+                        <List className="footer-links-list">
+                            <ListItem className="footer-links-list-item">
+                                <a href="">Disclaimer</a>
+                            </ListItem>
+                            <ListItem className="footer-links-list-item">
+                                <a href="">Privacy Policy</a>
+                            </ListItem>
+                            <ListItem className="footer-links-list-item">
+                                <a href="">Terms of Use</a>
+                            </ListItem>
+                        </List>
+                    </Container>
+                    <Container className="footer-text">
+                        Some of the pages on my travel blog contain affiliate
+                        links. Whenever you buy something through one of these
+                        links, I get a small commission at no extra cost to you.
+                        As an affiliate, I only recommend products and services
+                        that I feel are high quality and helpful to my readers.
+                        Thanks for your support.
+                    </Container>
+                </Container>
+            </Container>
+        </>
+    )
 }
 export default Footer
